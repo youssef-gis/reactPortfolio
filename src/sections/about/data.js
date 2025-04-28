a1df5eaa973edfe1a30e90f16a51756535596774
YOUSSEF <40327134+youssef-gis@users.noreply.github.com>
2025-04-28 15:26:30 +0100

    Update data.js

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -4,7 +4,7 @@ import {BiHappyHeartEyes} from 'react-icons/bi'
 
 
 const data = [
-    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '2+ Years Working'},
+    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '3+ Years Working'},
     {id: 2, icon: <TbBooks/>, title: 'Projects', desc: '4+ Completed'},
     {id: 3, icon: <BiHappyHeartEyes/>, title: 'Clients', desc: '6+ happy clients'}
     ]
