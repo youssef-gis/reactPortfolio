3c397546f9741441f23adee9e06831c7635e9b17
YOUSSEF <40327134+youssef-gis@users.noreply.github.com>
2025-07-11 01:29:54 +0100

    Update project.jsx

--- src/sections/portfolio/project.jsx
+++ src/sections/portfolio/project.jsx
@@ -10,7 +10,7 @@ const project = ({project}) => {
         <p>{project.desc}</p>
         <div className="portfolio__project-cta">
             <a href={project.github} className='btn sm primary' target='_blank' rel='noopener noreferrer'>Code</a>
-            { {project.demo} && <a href={project.demo} className='btn sm primary' target='_blank' rel='noopener noreferrer'>Demo</a>}
+            { {project.demo} && ( <a href={project.demo} className='btn sm primary' target='_blank' rel='noopener noreferrer'>Demo</a> ) }
         </div>
     </Card>
   )
